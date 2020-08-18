3d097cdabbc46bfb868ea4711ce81663b024cd6a
pzjyyzs <463669395@qq.com>
2020-08-18 22:25:15 +0800

    todo add

--- learn-one/src/components/Xiaojiejie.tsx
+++ learn-one/src/components/Xiaojiejie.tsx
@@ -2,19 +2,52 @@ import React from "react";
 
 export interface Props {}
 
-export interface State {}
+export interface State {
+  val: string;
+  list: Array<any>;
+}
 
 class Xiaojiejie extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      val: "",
+      list: ["anmo", "jingyou"],
+    };
+  }
+
+  inputChange = (e: any) => {
+    this.setState({
+      ...this.state,
+      val: e.target.value,
+    });
+  };
+  add = () => {
+    let { val, list } = this.state;
+    if (list.indexOf(val) === -1) {
+      list.push(val);
+    }
+    this.setState({
+      ...this.state,
+      list,
+    });
+  };
   render() {
     return (
       <React.Fragment>
         <div>
-          <input type="text" />
-          <button>123</button>
+          <input
+            type="text"
+            value={this.state.val}
+            onChange={this.inputChange}
+          />
+          <button onClick={this.add}>123</button>
           <ul>
-            <li>anmo</li>
-            <li>jingyou</li>
+            {this.state.list.map((item) => {
+              return <li key={item}>{item}</li>;
+            })}
           </ul>
+          <div>{this.state.val}</div>
         </div>
       </React.Fragment>
     );
