a46bce52653da5c8de3aedc816acd8a4d3e07f04
pzjyyzs <463669395@qq.com>
2020-08-24 23:34:22 +0800

    rouer-leacn

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, RediRedirect, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -25,7 +25,10 @@ class App extends Component {
             />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
+            <Redirect from="/messages" to="/posts"></Redirect>
+            <Route path="/not-found" component={NotFound} />
             <Route path="/" component={Home} />
+            <Redirect to="/no-found" />
           </Switch>
         </div>
       </div>
--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Users from "./users";
+import Posts from "./posts";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/admin/posts"></Link>
+        </li>
+        <li>
+          <Link to="/admin/users"></Link>
+        </li>
+      </ul>
+      <Route path="/admin/users" component={Users}></Route>
+      <Route path="/admin/posts" component={Posts}></Route>
     </div>
   );
 };
