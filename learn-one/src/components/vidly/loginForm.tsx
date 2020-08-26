2a94dfacff21452d2c34f9fab772af426394016e
pzjyyzs <463669395@qq.com>
2020-08-27 00:13:22 +0800

    validate

--- learn-one/src/components/vidly/loginForm.tsx
+++ learn-one/src/components/vidly/loginForm.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Component } from "react";
+import Input from "../common/input";
+import Joi from "joi";
 
 export interface LoginFormProps {}
 
@@ -8,48 +10,73 @@ export interface LoginFormState {}
 class LoginForm extends Component<LoginFormProps, LoginFormState> {
   state = {
     account: { username: "", password: "" },
+    errors: {},
   };
   /* username = React.createRef() as any; */
+
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
+  validate = () => {
+    (Joi as any).validate(this.state.account, this.schema);
+    const errors: any = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "") {
+      errors.username = "Username is required.";
+    }
+    if (account.password.trim() === "") {
+      errors.password = "password is required.";
+    }
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
   handleSubmit = (e: any) => {
     e.preventDefault();
 
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
     // const username = this.username.current.value;
   };
 
+  validateProperty = (input: any) => {
+    return "";
+  };
+
   handleChange = ({ currentTarget: input }: any) => {
+    const errors: any = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) {
+      errors[input.name] = errorMessage;
+    } else {
+      delete errors[input.name];
+    }
     const account: any = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              /* ref={() => this.username} */
-              value={account.username}
-              onChange={this.handleChange}
-              id="username"
-              name="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              name="password"
-              id="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+            error={(errors as any).username}
+          ></Input>
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={(errors as any).password}
+          ></Input>
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
