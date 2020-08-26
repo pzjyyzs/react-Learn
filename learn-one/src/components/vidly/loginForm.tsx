5b3dc89eff38b539ee307b12d1f38f90add6fb65
pzjyyzs <463669395@qq.com>
2020-08-26 23:15:14 +0800

    受控组件

--- learn-one/src/components/vidly/loginForm.tsx
+++ learn-one/src/components/vidly/loginForm.tsx
@@ -6,13 +6,23 @@ export interface LoginFormProps {}
 export interface LoginFormState {}
 
 class LoginForm extends Component<LoginFormProps, LoginFormState> {
-  username = React.createRef() as any;
+  state = {
+    account: { username: "", password: "" },
+  };
+  /* username = React.createRef() as any; */
   handleSubmit = (e: any) => {
     e.preventDefault();
 
-    const username = this.username.current.value;
+    // const username = this.username.current.value;
+  };
+
+  handleChange = ({ currentTarget: input }: any) => {
+    const account: any = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -20,15 +30,25 @@ class LoginForm extends Component<LoginFormProps, LoginFormState> {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              ref={() => this.username}
+              /* ref={() => this.username} */
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
