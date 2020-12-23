489f8c4c34e2381b360cfbb2ead1b01f6c8ff835
pzjyyzs <463669395@qq.com>
2020-12-23 23:44:38 +0800

    redux 准备

--- music/src/application/Home/Home.tsx
+++ music/src/application/Home/Home.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
+import { NavLink } from 'react-router-dom';
+import { Tab, TabItem, Top } from './style';
 
 const Home = (props: any) => {
     const { route }  = props;
     console.log(props)
     return (
         <div>
-            Home
+            <Top>
+                <span className="iconfont menu">&#xe65c;</span>
+                <span className="title">WebApp</span>
+                <span className="iconfont search">&#xe62b;</span>
+            </Top>
+            <Tab>
+                <NavLink to="/recommend" activeClassName="selected">
+                    <TabItem>
+                        <span>推荐</span>
+                    </TabItem>
+                </NavLink>
+                <NavLink to="/singers" activeClassName="selected">
+                    <TabItem>
+                        <span>歌手</span>
+                    </TabItem>
+                </NavLink>
+                <NavLink to="/rank" activeClassName="selected">
+                    <TabItem>
+                        <span>排行榜</span>
+                    </TabItem>
+                </NavLink>
+            </Tab>
             { renderRoutes(route.routes) }
         </div>
     );
